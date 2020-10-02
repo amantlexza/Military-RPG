bad7eecf516fcbfe48a8efab48bf4345eb34528f
Nosimphiwe <nomsimbenge2@gmail.com>
2020-10-02 23:19:00 +0200

    Flight Simulation Attempt

--- Military_RPG/Form1.Designer.cs
+++ Military_RPG/Form1.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.plane = new System.Windows.Forms.PictureBox();
+            this.militaryBase = new System.Windows.Forms.PictureBox();
+            this.enemySquad = new System.Windows.Forms.PictureBox();
+            this.enemyCamp = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.plane)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.militaryBase)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemySquad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyCamp)).BeginInit();
             this.SuspendLayout();
+            // 
+            // plane
+            // 
+            this.plane.Image = ((System.Drawing.Image)(resources.GetObject("plane.Image")));
+            this.plane.Location = new System.Drawing.Point(146, 367);
+            this.plane.Name = "plane";
+            this.plane.Size = new System.Drawing.Size(93, 72);
+            this.plane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.plane.TabIndex = 0;
+            this.plane.TabStop = false;
+            // 
+            // militaryBase
+            // 
+            this.militaryBase.Image = ((System.Drawing.Image)(resources.GetObject("militaryBase.Image")));
+            this.militaryBase.Location = new System.Drawing.Point(332, 58);
+            this.militaryBase.Name = "militaryBase";
+            this.militaryBase.Size = new System.Drawing.Size(93, 72);
+            this.militaryBase.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.militaryBase.TabIndex = 1;
+            this.militaryBase.TabStop = false;
+            // 
+            // enemySquad
+            // 
+            this.enemySquad.Image = ((System.Drawing.Image)(resources.GetObject("enemySquad.Image")));
+            this.enemySquad.Location = new System.Drawing.Point(161, 171);
+            this.enemySquad.Name = "enemySquad";
+            this.enemySquad.Size = new System.Drawing.Size(93, 72);
+            this.enemySquad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemySquad.TabIndex = 2;
+            this.enemySquad.TabStop = false;
+            // 
+            // enemyCamp
+            // 
+            this.enemyCamp.Image = ((System.Drawing.Image)(resources.GetObject("enemyCamp.Image")));
+            this.enemyCamp.Location = new System.Drawing.Point(332, 225);
+            this.enemyCamp.Name = "enemyCamp";
+            this.enemyCamp.Size = new System.Drawing.Size(93, 72);
+            this.enemyCamp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemyCamp.TabIndex = 3;
+            this.enemyCamp.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 450);
+            this.ClientSize = new System.Drawing.Size(766, 506);
+            this.Controls.Add(this.plane);
+            this.Controls.Add(this.enemyCamp);
+            this.Controls.Add(this.enemySquad);
+            this.Controls.Add(this.militaryBase);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.plane)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.militaryBase)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemySquad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyCamp)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox plane;
+        private System.Windows.Forms.PictureBox militaryBase;
+        private System.Windows.Forms.PictureBox enemySquad;
+        private System.Windows.Forms.PictureBox enemyCamp;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Military_RPG/Form1.cs
+++ Military_RPG/Form1.cs
@@ -12,9 +12,175 @@ namespace Military_RPG
 {
     public partial class Form1 : Form
     {
+        int x, y, bCenter, bMiddle, pCenter, pMiddle, sCenter, cCenter, speed;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            speed = 1;
+            //plane.Height will be distance
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            militaryBase.Top += 1;
+            enemyCamp.Top += 1;
+            enemySquad.Top += 1;
+
+            bCenter = militaryBase.Left + militaryBase.Width / 2;
+            bMiddle = militaryBase.Bottom + militaryBase.Height / 2;
+            pCenter = plane.Left + plane.Width / 2;
+            pMiddle = plane.Bottom + plane.Height / 2;
+            sCenter = enemySquad.Left + enemySquad.Width / 2;
+            cCenter = enemyCamp.Left + enemyCamp.Width / 2;
+
+            if ((plane.Left <= militaryBase.Right && plane.Left >= militaryBase.Left)  || ((plane.Right <= militaryBase.Right && plane.Right >= militaryBase.Left)))
+            {
+                //qualifies as straight ahead
+                StraightAhead(sCenter, bCenter, cCenter);
+            }
+            else if (plane.Left < militaryBase.Left)
+            {
+                //qualifies as diagonal ahead but base is right
+                DiagonalToRight(pCenter,bCenter,pMiddle,bMiddle);
+                
+            }
+            else if (plane.Right >= militaryBase.Right)
+            {
+                //qualifies as diagonal ahead but base is left
+                DiagonalToLeft(pCenter,bCenter,pMiddle,bMiddle);
+            }
+
+            if (pMiddle == bMiddle)
+            {
+                if ((plane.Left <= militaryBase.Right && plane.Left >= militaryBase.Left) || ((plane.Right <= militaryBase.Right && plane.Right >= militaryBase.Left)))
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Arrived successfully");
+                }
+            }
+
+        }
+        private void StraightAhead(int sCenter, int bCenter, int cCenter) {
+            
+            if ((plane.Top - plane.Height) <= (enemySquad.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemySquad.Top)
+            {
+                //plane.Top += MinSpeed(speed);
+
+                //check for squad close proximity
+                if (plane.Left >= (enemySquad.Left - plane.Width) && plane.Right <= (enemySquad.Right + plane.Width))
+                {
+                    //bullet
+                    //LeftOrRight(sCenter,bCenter,enemySquad.Right,enemySquad.Left,speed);
+                }
+            }
+            else if ((plane.Top - plane.Height) <= (enemyCamp.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemyCamp.Top)
+            {
+                //check for camp close proximity
+                if (plane.Left >= (enemyCamp.Left - plane.Width) && plane.Right <= (enemyCamp.Right + plane.Width))
+                {
+                    //bullet
+                    //LeftOrRight(cCenter, bCenter, enemyCamp.Right, enemyCamp.Left,speed);
+                }
+            }
+        }
+        public void DiagonalToRight(int pCenter, int bCenter,int pMiddle,int bMiddle) {
+            x = Math.Abs(bCenter - pCenter);
+            y = Math.Abs(pMiddle - bMiddle);
+            speed = x / y;
+            plane.Left += MinSpeed(speed);
+
+            if ((plane.Top - plane.Height) <= (enemySquad.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemySquad.Top)
+            {
+                //check for squad close proximity
+                if (plane.Left >= (enemySquad.Left - plane.Width) && plane.Right <= (enemySquad.Right + plane.Width))
+                {
+                    //MessageBox.Show("squad");
+                    //bullet
+                    // LeftOrRight(sCenter, bCenter, enemySquad.Right, enemySquad.Left,speed);
+                }
+                else
+                {
+                    //no bullet
+                }
+            }
+            else if ((plane.Top - plane.Height) <= (enemyCamp.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemyCamp.Top)
+            {
+                //check for camp close proximity
+                if (plane.Left >= (enemyCamp.Left - plane.Width) && plane.Right <= (enemyCamp.Right + plane.Width))
+                {
+                    MessageBox.Show("camp");
+                    //bullet
+                    //LeftOrRight(cCenter, bCenter, enemyCamp.Right, enemyCamp.Left,speed);
+                }
+                else
+                {
+                    //no bullet
+                }
+            }
+        }
+        public void DiagonalToLeft(int pCenter, int bCenter, int pMiddle, int bMiddle)
+        {
+            x = Math.Abs(pCenter - bCenter);
+            y = Math.Abs(pMiddle - bMiddle);
+            speed = x / y;
+            plane.Left -= MinSpeed(speed);
+
+            if ((plane.Top - plane.Height) <= (enemySquad.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemySquad.Top)
+            {
+                //check for squad close proximity
+                if (plane.Left >= (enemySquad.Left - plane.Width) && plane.Right <= (enemySquad.Right + plane.Width))
+                {
+                    //MessageBox.Show("squad close");
+                    //bullet
+                    // LeftOrRight(sCenter, bCenter, enemySquad.Right, enemySquad.Left,speed);
+                }
+                else
+                {
+                    //no bullet
+                }
+            }
+            else if ((plane.Top - plane.Height) <= (enemyCamp.Bottom + plane.Height) && (plane.Top - plane.Height) >= enemyCamp.Top)
+            {
+                //check for camp close proximity
+                if (plane.Left >= (enemyCamp.Left - plane.Width) && plane.Right <= (enemyCamp.Right + plane.Width))
+                {
+                    //MessageBox.Show("camp close");
+                    //bullet
+                    //LeftOrRight(cCenter, bCenter, enemyCamp.Right, enemyCamp.Left,speed);
+                }
+                else
+                {
+                    //no bullet
+                }
+            }
+        }
+        public void LeftOrRight(int eCenter,int bCenter,int enemyRight,int enemyLeft,int speed) {
+            //check if best plane movement is left or right
+            if (eCenter <= bCenter)
+            {
+                //plane.Left += speed;
+                plane.Left = enemyRight + plane.Width + 1;
+            }
+            else
+            {
+                //plane.Left -= MinSpeed(speed);
+                plane.Left = enemyLeft - plane.Width - 1;
+            }
+        }
+        public int MinSpeed(int speed) {
+            if (speed<1)
+            {
+                return 1;
+            }
+            else
+            {
+                return speed;
+            }
         }
     }
 }
